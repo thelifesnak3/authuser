5809b25575febdd6d746cdc59a19a7687899341b
Felipe Alexandre <felipe.silva@petlove.com.br>
2022-04-03 10:32:19 -0300

    added pageable at findAll user

--- src/main/java/com/ead/authuser/services/UserService.java
+++ src/main/java/com/ead/authuser/services/UserService.java
@@ -1,6 +1,8 @@
 package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +11,8 @@ import java.util.UUID;
 public interface UserService {
 
     List<UserModel> findAll();
+
+    Page<UserModel> findAll(Pageable pageable);
 
     Optional<UserModel> findById(UUID id);
 
